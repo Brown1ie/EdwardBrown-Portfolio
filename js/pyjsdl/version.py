14dcb8b67e5212cb7626c115020491824845f245
Brown1ie <edward.a.brown@icloud.com>
2025-02-26 15:28:01 +0000

    returned to old demo system

--- js/pyjsdl/version.py
+++
@@ -1,8 +0,0 @@
-#Pyjsdl - Copyright (C) 2013 James Garnon <https://gatc.ca/>
-#Released under the MIT License <https://opensource.org/licenses/MIT>
-
-from pyjsdl import __version__
-
-
-ver = __version__
-
